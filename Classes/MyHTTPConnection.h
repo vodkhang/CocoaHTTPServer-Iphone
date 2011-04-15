c84d95db959e243dfc103928250fda67a336ee07
Khang Vo <vodkhang@gmail.com>
2011-04-16 01:37:07 +0930

    add the whole project in

---
+++ Classes/MyHTTPConnection.h
@@ -0,0 +1,24 @@
+//
+//  This class was created by Nonnus,
+//  who graciously decided to share it with the CocoaHTTPServer community.
+//
+
+#import <Foundation/Foundation.h>
+#import "HTTPConnection.h"
+
+
+@interface MyHTTPConnection : HTTPConnection
+{
+    // vodkhang 
+    // Delete these lines 
+//	int dataStartIndex;
+//	NSMutableArray* multipartData;
+//	BOOL postHeaderOK;
+}
+
+- (BOOL)isBrowseable:(NSString *)path;
+- (NSString *)createBrowseableIndex:(NSString *)path;
+
+//- (BOOL)supportsPOST:(NSString *)path withSize:(UInt64)contentLength;
+
+@end
